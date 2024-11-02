0ab6e2093193bcb005e079fb3b6869ee22e763fc
Iryna <123497292+SkhtskIryna@users.noreply.github.com>
2024-11-02 16:28:51 +0200

    Update suite.cpp

--- suite.cpp
+++ suite.cpp
@@ -6,7 +6,9 @@ double Suite::FuncA(int n, double x) {
   double sum = 0; //The initial value of the amount
 
   for(int i = 1; i <= n; ++i) {
-    sum += ((std:pow(-1, i) * std:tgamma(2*i+1)) / ((1-2*i) * std::tgamma(i+1) * std::tgamma(i+1) * std::pow(4, i))* std::pow(x, i);
+       sum += ((std::pow(-1, i) * std::tgamma(2*i+1)) /
+         ((1 - 2*i) * std::tgamma(i+1) * std::tgamma(i+1) * std::pow(4, i)) *
+         std::pow(x, i));
   }
   return sum;
 }
